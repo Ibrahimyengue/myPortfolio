8392ceb48e66ca87d46770ac733fea22c15dc49f
Ibrahima bah <25622050+Ibrahimyengue@users.noreply.github.com>
2019-11-20 22:51:02 -0500

    Updates

---
+++ precache-manifest.0bfaa17eaa247072f768f0d56414f537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e7fe2fdb12393ae629f",
+    "url": "/myPortfolio/static/css/main.e9eb323b.chunk.css"
+  },
+  {
+    "revision": "3e7fe2fdb12393ae629f",
+    "url": "/myPortfolio/static/js/main.3e7fe2fd.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/myPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b0ac8112640d8ddb0d56",
+    "url": "/myPortfolio/static/js/runtime~main.b0ac8112.js"
+  },
+  {
+    "revision": "afa1928f28e102da84ba5568183f8d1a",
+    "url": "/myPortfolio/index.html"
+  }
+];
